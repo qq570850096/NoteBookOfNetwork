d481d6fccd4b1b2dd6ff9100186f67e8f5a442a0
exia <someone741@qq.com>
2020-06-24 21:11:27 +0800

    新增修改

--- 软件测试大作业/gateway_demo/demo/NextDay/nextDay_test.go
+++ 软件测试大作业/gateway_demo/demo/NextDay/nextDay_test.go
@@ -1,16 +1,110 @@
 package NextDay
 
 import (
-	"strconv"
 	"testing"
 )
 
-func TestBigMonth(t *testing.T) {
-	f := Unit{}
-	year,month,day := 2008,12,0
+var util Unit
 
-	s1 := strconv.Itoa(year)
-	s2 := strconv.Itoa(month)
-	s3 := strconv.Itoa(day)
-	f.NextDay(s1,s2,s3)
+func TestBigMonth(t *testing.T) {
+	TestStmtCover(t)
+	TestDesicionCover(t)
+	TestBoundaryOfNextDay(t)
+	TestDesicionTableOfNextDay(t)
+	TestPathCover(t)
+	TestEClassOfNextDay(t)
+}
+
+func TestBoundaryOfNextDay(t *testing.T) {
+	t.Log("以下是次日问题的边界值用例测试结果:")
+	util.NextDay("1811", "6", "15")
+	util.NextDay("1812", "6", "15")
+	util.NextDay("1813", "6", "15")
+	util.NextDay("1912", "6", "15")
+	util.NextDay("2011", "6", "15")
+	util.NextDay("2012", "6", "15")
+	util.NextDay("2013", "6", "15")
+	util.NextDay("1912", "6", "-1")
+	util.NextDay("1912", "6", "1")
+	util.NextDay("1912", "6", "2")
+	util.NextDay("1912", "6", "29")
+	util.NextDay("1912", "6", "30")
+	util.NextDay("1912", "6", "31")
+	util.NextDay("1912", "6", "32")
+	util.NextDay("1912", "-1", "15")
+	util.NextDay("1912", "1", "15")
+	util.NextDay("1912", "2", "15")
+	util.NextDay("1912", "11", "15")
+	util.NextDay("1912", "12", "15")
+	util.NextDay("1912", "13", "15")
+	t.Log("边界值用例测试通过")
+}
+func TestEClassOfNextDay(t *testing.T) {
+	t.Log("开始次日问题等价类测试结果:")
+	util.NextDay("1812", "6", "30")
+	util.NextDay("2012", "6", "30")
+	util.NextDay("2000", "2", "29")
+	util.NextDay("1912", "7", "31")
+	util.NextDay("1909", "2", "28")
+	util.NextDay("2005", "10", "31")
+	util.NextDay("1997", "11", "31")
+	util.NextDay("1999", "12", "31")
+	util.NextDay("1900", "2", "29")
+	util.NextDay("abcd", "6", "6")
+	util.NextDay("6.6", "6", "6")
+	util.NextDay("1787", "6", "5")
+	util.NextDay("2018", "7", "2")
+	util.NextDay("2000", "0", "6")
+	util.NextDay("2000", "13", "6")
+	util.NextDay("2000", "6", "0")
+	util.NextDay("2000", "6", "32")
+	util.NextDay("1887", "1", "15")
+	util.NextDay("", "", "")
+	t.Log("等价类测试通过")
+}
+func TestDesicionTableOfNextDay(t *testing.T) {
+	t.Log("开始次日问题的决策表测试结果:")
+	util.NextDay("2007", "6", "15")
+	util.NextDay("2007", "6", "30")
+	util.NextDay("2007", "6", "31")
+	util.NextDay("2007", "1", "15")
+	util.NextDay("2007", "1", "31")
+	util.NextDay("2007", "12", "15")
+	util.NextDay("2007", "12", "31")
+	util.NextDay("2007", "2", "15")
+	util.NextDay("2007", "2", "28")
+	util.NextDay("2007", "2", "29")
+	util.NextDay("2007", "2", "30")
+	util.NextDay("2000", "2", "28")
+	util.NextDay("2000", "2", "29")
+	util.NextDay("2000", "2", "30")
+	t.Log("决策表测试通过")
+}
+func TestStmtCover(t *testing.T) {
+	t.Log("开始次日问题语句覆盖的测试:")
+	util.NextDay("2000", "2", "29")
+	util.NextDay("2001", "2", "28")
+	util.NextDay("2000", "3", "28")
+	util.NextDay("2001", "12", "31")
+	util.NextDay("2001", "11", "30")
+	t.Log("测试通过")
+}
+func TestDesicionCover(t *testing.T) {
+	t.Log("开始次日问题判定覆盖的测试:")
+	util.NextDay("2000", "2", "29")
+	util.NextDay("2001", "2", "28")
+	util.NextDay("2000", "3", "28")
+	util.NextDay("2001", "12", "31")
+	util.NextDay("2001", "11", "30")
+	t.Log("语句覆盖测试通过")
+}
+func TestPathCover(t *testing.T) {
+	t.Log("开始次日问题路径覆盖的测试:")
+	util.NextDay("2008", "2", "29")
+	util.NextDay("2008", "7", "7")
+	util.NextDay("2008", "12", "31")
+	util.NextDay("2008", "11", "30")
+	util.NextDay("2007", "10", "31")
+	util.NextDay("2007", "2", "28")
+	t.Log("路径覆盖测试通过")
 }
--- 软件测试大作业/gateway_demo/demo/test_exam/Triangle_test.go
+++ 软件测试大作业/gateway_demo/demo/test_exam/Triangle_test.go
@@ -1,12 +1,20 @@
 package test_exam
 
-import "testing"
+import (
+	"testing"
+)
 
 var util Triangle
 
 func TestTriangle(t *testing.T) {
 	TestTriangle_IsTriangle(t)
 	TestGeneralBoundaryOfTriangle(t)
+	TestConditionCover(t)
+	TestDesicionCover(t)
+	TestEClassOfTriangle(t)
+	TestDesicionTableOfTriangle(t)
+	TestRobustBoundaryOfTriangle(t)
+	TestStmtCover(t)
 }
 
 func TestTriangle_IsTriangle(t *testing.T) {
@@ -37,4 +45,110 @@ func TestGeneralBoundaryOfTriangle(t *testing.T){
 	util.IsTriangle("2", "60", "60")
 	util.IsTriangle("99", "50", "50")
 	util.IsTriangle("100", "50", "50")
+	t.Log("一般边界测试成功！")
+}
+
+func TestRobustBoundaryOfTriangle(t *testing.T) {
+	t.Log("这是三角形问题健壮性边界值测试的结果:")
+	util.IsTriangle("0", "50", "50")
+	util.IsTriangle("1", "50", "50")
+	util.IsTriangle("2", "50", "50")
+	util.IsTriangle("50", "50", "50")
+	util.IsTriangle("99", "50", "50")
+	util.IsTriangle("100", "50", "50")
+	util.IsTriangle("101", "50", "50")
+	util.IsTriangle("50", "0", "50")
+	util.IsTriangle("50", "1", "50")
+	util.IsTriangle("50", "2", "50")
+	util.IsTriangle("50", "99", "50")
+	util.IsTriangle("50", "100", "50")
+	util.IsTriangle("50", "101", "50")
+	util.IsTriangle("50", "50", "0")
+	util.IsTriangle("50", "50", "1")
+	util.IsTriangle("50", "50", "2")
+	util.IsTriangle("50", "50", "99")
+	util.IsTriangle("50", "50", "100")
+	util.IsTriangle("50", "50", "101")
+	t.Log("健壮性边界值测试成功")
+}
+
+func TestEClassOfTriangle(t *testing.T){
+	t.Log("这是三角形问题等价类划分的结果:")
+	util.IsTriangle("2", "3", "4")
+	util.IsTriangle("2", "2", "3")
+	util.IsTriangle("2", "3", "2")
+	util.IsTriangle("3", "2", "2")
+	util.IsTriangle("2", "2", "2")
+	util.IsTriangle("1", "2", "3")
+	util.IsTriangle("2.1", "3", "4")
+	util.IsTriangle("2.1", "2.1", "3")
+	util.IsTriangle("2.1", "2.1", "2.1")
+	util.IsTriangle("0", "2", "3")
+	util.IsTriangle("0", "0", "2")
+	util.IsTriangle("0", "0", "0")
+	util.IsTriangle("-1", "2", "2")
+	util.IsTriangle("-1", "-2", "2")
+	util.IsTriangle("-2", "-2", "-2")
+	util.IsTriangle("111", "99", "99")
+	util.IsTriangle("111", "111", "99")
+	util.IsTriangle("111", "111", "111")
+	util.IsTriangle("5", "3", "2")
+	util.IsTriangle("5", "2", "3")
+	util.IsTriangle("2", "5", "3")
+	util.IsTriangle("", "", "")
+	util.IsTriangle("2", "3", "")
+	util.IsTriangle("3", "4", "5")
+	t.Log("等价类划分测试成功！")
+}
+
+func TestDesicionTableOfTriangle(t *testing.T){
+	t.Log("这是三角形问题决策表测试的结果:")
+	util.IsTriangle("4", "1", "2")
+	util.IsTriangle("1", "4", "2")
+	util.IsTriangle("1", "2", "4")
+	util.IsTriangle("5", "5", "5")
+	util.IsTriangle("?", "?", "?")
+	util.IsTriangle("2", "2", "3")
+	util.IsTriangle("2", "3", "2")
+	util.IsTriangle("3", "2", "2")
+	util.IsTriangle("3", "4", "5")
+	t.Log("决策表测试成功")
+}
+
+func TestStmtCover(t *testing.T){
+	t.Log("这是三角形问题语句覆盖的结果:")
+	util.IsTriangle("-2", "3", "4")
+	util.IsTriangle("A", "3", "4")
+	util.IsTriangle("3.1", "3", "4")
+	util.IsTriangle("2", "3", "4")
+	util.IsTriangle("2", "2", "3")
+	util.IsTriangle("2", "2", "2")
+	util.IsTriangle("", "", "")
+	t.Log("语句覆盖测试通过")
+}
+
+func TestDesicionCover(t *testing.T) {
+	t.Log("这是三角形问题判定覆盖的结果:")
+	util.IsTriangle("50", "50", "50")
+	util.IsTriangle("1", "2", "3")
+	util.IsTriangle("3.14", "5", "6")
+	util.IsTriangle("A", "5", "6")
+	util.IsTriangle("2", "2", "3")
+	util.IsTriangle("4", "5", "6")
+	t.Log("问题判定覆盖测试通过")
+}
+
+func TestConditionCover(t *testing.T) {
+	t.Log("这是三角形问题条件覆盖的结果:")
+	util.IsTriangle("-2", "2", "4")
+	util.IsTriangle("3", "-4", "120")
+	util.IsTriangle("3", "5", "9")
+	util.IsTriangle("8", "1", "3")
+	util.IsTriangle("2", "8", "4")
+	util.IsTriangle("3", "3", "3")
+	util.IsTriangle("2", "3", "4")
+	util.IsTriangle("2", "2", "3")
+	util.IsTriangle("3", "4", "3")
+	util.IsTriangle("4", "5", "5")
+	t.Log("条件覆盖测试通过")
 }
